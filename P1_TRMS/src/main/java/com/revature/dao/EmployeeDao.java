89491a6dd277f49ee70c24065f45b50407aed5c7
jtodd-3 <james.todd927@gmail.com>
2019-11-06 16:16:59 -0500

    finished EmployeeDaoImpl and FormDaoImpl

--- P1_TRMS/src/main/java/com/revature/dao/EmployeeDao.java
+++ P1_TRMS/src/main/java/com/revature/dao/EmployeeDao.java
@@ -6,5 +6,5 @@ import com.revature.beans.Employee;
 
 public interface EmployeeDao {
 	public Employee getEmployeeById(int id) throws SQLException;
-	public void InsertEmployee(Employee e) throws SQLException;
+	public void insertEmployee(Employee e) throws SQLException;
 }
--- P1_TRMS/src/main/java/com/revature/daoImpl/EmployeeDaoImpl.java
+++ P1_TRMS/src/main/java/com/revature/daoImpl/EmployeeDaoImpl.java
@@ -1,31 +1,53 @@
 package com.revature.daoImpl;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.revature.beans.Employee;
 import com.revature.dao.EmployeeDao;
 import com.revature.util.ConnFactory;
 
-public class EmployeeDaoImpl implements EmployeeDao{
+public class EmployeeDaoImpl implements EmployeeDao {
 	ConnFactory cf = ConnFactory.getInstance();
-	
+
 	public Employee getEmployeeById(int id) throws SQLException {
+		//Declare connection variables
 		Connection conn = cf.getConnection();
 		Employee e = null;
 		String sql = "";
 		PreparedStatement ps = conn.prepareStatement(sql);
-		
-		
-		
-		return e;
-	}
 
-	public void InsertEmployee(Employee e) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-	
-}
+		//Query for employee by id and return
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			e = new Employee(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+					rs.getString(6), rs.getDouble(7));
+		}
+		return e;
+	}//End of method getEmployeeById
+
+	public void insertEmployee(Employee e) throws SQLException {
+		//Declare connection variables
+		Connection conn = cf.getConnection();
+		String sql = "";
+		PreparedStatement ps = conn.prepareStatement(sql);
+
+		//Insert data from employee into database
+		ps.setInt(1, e.getEmp_id());
+		ps.setString(2, e.getfName());
+		ps.setString(3, e.getlName());
+		ps.setString(4, e.getDeptName());
+		ps.setString(5, e.getUser());
+		ps.setString(6, e.getPw());
+		ps.setDouble(7, e.getTotalReimbursements());
+		ps.execute();
+
+		//Commit changes to database
+		ps = conn.prepareStatement("commit");
+		ps.execute();
+
+	}//End of method InsertEmployee
+
+}//End of Class EmployeeDaoImpl
