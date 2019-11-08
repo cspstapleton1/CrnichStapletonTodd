19fbcaf4211760f2b61e31ab0fe346e34ec85b0f
Ryancrnich <Ryancrnich@gmail.com>
2019-11-08 11:51:44 -0500

    js added

--- input.js
+++ input.js
@@ -38,3 +38,35 @@ c.value = 30
 b.value = 'Other'
 d[0].type="radio"
 });
+
+
+function submitApp(){
+ let vg=document.getElementById("reinbursmentForm").submit;
+ var xhr= new XMLHttpRequest();
+ xhr.onreadystatechange= function() {
+if(xhr.readyState==4 && xhr.status==200){
+ console.log(xhr.responseText);
+ }
+ }
+xhr.open("POST","http://localhost:8080/review/app",true);
+ var payload=jsonBuilder();
+ xhr.send(payload);
+
+}
+function jsonBuilder() {
+var elements = document.getElementById("reinbursmentForm").elements;
+ var obj ={};
+ for(var i = 0 ; i < elements.length-1; i++){
+    var item = elements.item(i);
+    obj[item.name] = item.value;
+    console.log(obj);   
+ }
+var json= JSON.stringify(obj);
+console.log(json);
+return json;
+}
+
+window.onload= function() {
+ document.getElementById("reinbursmentSubmit").addEventListener("click",submitApp,false);
+// document.getElementById("vgFormSubmit").addEventListener("click", postVG,false);
+}
---
+++ login.js
@@ -0,0 +1,30 @@
+function userLogin(){
+    let vg=document.getElementById("login").submit;
+    var xhr= new XMLHttpRequest();
+    xhr.onreadystatechange= function() {
+   if(xhr.readyState==4 && xhr.status==200){
+    console.log(xhr.responseText);
+    }
+    }
+   xhr.open("POST","file:///C:/Users/Revature%20Associate/Documents/login.html",true);
+    var payload=jsonBuilder();
+    xhr.send(payload);
+   
+   }
+   
+   function jsonBuilder() {
+   var elements = document.getElementById("login").elements;
+    var obj ={};
+    for(var i = 0 ; i < elements.length-1; i++){
+       var item = elements.item(i);
+       obj[item.name] = item.value;
+       console.log(obj);   
+    }
+   var json= JSON.stringify(obj);
+   console.log(json);
+   return json;
+   }
+   
+   window.onload= function() {
+    document.getElementById("loginSubmit").addEventListener("click",userLogin,false);
+   }
