3f57b1578f12b3aa25d31f6c1e4a8f622b5c16b7
jtodd-3 <james.todd927@gmail.com>
2019-11-09 10:48:20 -0500

    updates and added servlets

--- P1_TRMS/src/main/java/com/revature/servlets/Login.java
+++ P1_TRMS/src/main/java/com/revature/servlets/Login.java
@@ -1,17 +1,62 @@
 package com.revature.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.revature.beans.Employee;
+import com.revature.daoImpl.EmployeeDaoImpl;
 
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
 
-}
+	public void init() throws ServletException{
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("In doPost");
+		EmployeeDaoImpl edi = new EmployeeDaoImpl();
+		Employee e = null;
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String name = request.getParameter("user");
+		String password = request.getParameter("pw");
+
+		try {
+			e = edi.login(name, password);
+			if (e.getUser() != null) {
+				HttpSession session = request.getSession();
+				e = edi.getEmployeeById(e.getEmp_id());
+				session.setAttribute("username", name);
+				session.setAttribute("Reimbursements", e.getTotalReimbursements());
+				session.setAttribute("id", e.getEmp_id());
+				session.setAttribute("lName", e.getlName());
+				session.setAttribute("fName", e.getfName());
+				session.setAttribute("Dept", e.getDeptName());
+				response.sendRedirect("forms");
+				request.getRequestDispatcher("forms.html").forward(request, response);
+			} else {
+				out.print("Incorrect login credentials. Please try again.");
+				request.getRequestDispatcher("login.html").forward(request, response);
+			}
+			out.close();
+		} catch (SQLException e1) {
+			System.out.println("You broke it.");
+			e1.printStackTrace();
+		}
+	}// End of method doPost
+
+}//End of Servlet Login 
--- P1_TRMS/src/main/java/com/revature/util/ConnFactory.java
+++ P1_TRMS/src/main/java/com/revature/util/ConnFactory.java
@@ -1,13 +1,8 @@
 package com.revature.util;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnFactory {
 	private static ConnFactory cf = new ConnFactory();
@@ -26,24 +21,15 @@ public class ConnFactory {
 	public Connection getConnection() {
 		// Declarations for connection variables
 				Connection conn = null;
-				Properties prop = new Properties();
-				File connFile = new File("src/main/resources/database.properties");
-
-				// Try to connect to the server. If fail, check for what broke
+				String url = "jdbc:postgresql://todd-pega1019.cwn0gwrwwhuo.us-east-2.rds.amazonaws.com/postgres";
+				String user = "jtodd3";
+				String pw = "William3545!";
 				try {
-					prop.load(new FileReader(connFile));
-					// Using Properties file
-					conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"),
-							prop.getProperty("password"));
+					conn = DriverManager.getConnection(url,user,pw);
 				} catch (SQLException e) {
 					System.out.println("Failed to create connection");
 					e.printStackTrace();
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}//End of try/catch
-				
+				}
 				return conn;
 	}//End of method getConnection
 }//End of Class ConnFactory
