13438b4f46a3c669acbdc9f20284554fb45c2a55
Ryancrnich <Ryancrnich@gmail.com>
2019-11-10 16:32:44 -0500

    final

--- apps.js
+++ apps.js
@@ -1,39 +1,36 @@
-function getapps(appArray){	
-    date = document.querySelectorAll("#submitDate")
-    start = document.querySelectorAll("#sDate")
-    end = document.querySelectorAll("#eDate")
-    end = document.querySelectorAll("#status")
-    let index = 0;
-    if(appArray.length==0){}
-if (appArray.length<5){
-    index =5;
-}
-else{
-    index = appArray.length;
-}
-    for(var i = 0; i < index ; i++){
-        date[i].innerHTML = appArray[i].submitDate;
-        start[i].innerHTML = appArray[i].sDate;
-        end[i].innerHTML = appArray.eDate[i];
-        status[i].innerHTML = appArray.status[i];
-    }
-    
-}
-function getapp(){
-console.log( "in get VG bananaphone!");
-let vgid=document.getElementById("appId").value;
+
+function getapps(){
+let vgid=document.getElementById("user").value;
 var xhr= new XMLHttpRequest();
  xhr.onreadystatechange= function() {
     console.log("in ORSC "+ xhr.readyState );
     if(xhr.readyState==4 && xhr.status==200){
        console.log(xhr.responseText);
-      var app= JSON.parse(xhr.responseText);
-        loadVG(app);
+      var apps= JSON.parse(xhr.responseText);
+      date = document.querySelectorAll("#submitDate")
+    start = document.querySelectorAll("#sDate")
+    end = document.querySelectorAll("#eDate")
+    end = document.querySelectorAll("#status")
+    let index = 0;
+    if(appArray.length==0){}
+else if (appArray.length<5){
+    index =5;
+}
+else{
+    index = appArray.length;
+}
+      for(var i = 0; i < index; i++){
+        date[i].innerHTML = appArray[i].submitDate;
+        start[i].innerHTML = appArray[i].sDate;
+        end[i].innerHTML = appArray.eDate[i];
+        status[i].innerHTML = appArray.status[i];
+      }
+  
     }
 }}
-function getTable(){
+function getapp(){
     console.log( "in get VG bananaphone!");
-    let vgid=document.getElementById("id").value;// get form session 
+    let vgid=document.getElementById("appId").value;
     var xhr= new XMLHttpRequest();
      xhr.onreadystatechange= function() {
         console.log("in ORSC "+ xhr.readyState );
@@ -43,10 +40,13 @@ function getTable(){
             loadVG(app);
         }
     }
+    
+
 xhr.open("GET","http://localhost:8080/JSONDemo/vg?vgid=" + vgid,true);
  xhr.send();
  }
 
  window.onload = function() {
-    getapps()
+    getapps();
+    document.getElementById("getapp").addEventListener("click",getapp,false);
  }
