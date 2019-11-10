a6f13ca5f68ecd0a4dc370963bd36269154cde05
jtodd-3 <james.todd927@gmail.com>
2019-11-10 17:11:44 -0500

    Cleaned up master branch and deleted unnecessary files

--- P1_TRMS/src/main/webapp/apps.js
+++ P1_TRMS/src/main/webapp/apps.js
@@ -1,27 +1,52 @@
-function loadVG(app){	
-    date = document.querySelectorAll("#submitDate")
-    start = document.querySelectorAll("#sDate")
-    end = document.querySelectorAll("#eDate")
 
-    for(var i = 0; i < 5; i++){
-        date[i].innerHTML = app.submitDate;
-        start[i].innerHTML = app.sDate;
-        end[i].innerHTML = app.eDate;
-    }
-    
-}
-function getapp(){
-console.log( "in get VG bananaphone!");
-let vgid=document.getElementById("vgIDInput").value;
+function getapps(){
+let vgid=document.getElementById("user").value;
 var xhr= new XMLHttpRequest();
  xhr.onreadystatechange= function() {
     console.log("in ORSC "+ xhr.readyState );
     if(xhr.readyState==4 && xhr.status==200){
        console.log(xhr.responseText);
-      var app= JSON.parse(xhr.responseText);
-        loadVG(app);
-    }
+      var apps= JSON.parse(xhr.responseText);
+      date = document.querySelectorAll("#submitDate")
+    start = document.querySelectorAll("#sDate")
+    end = document.querySelectorAll("#eDate")
+    end = document.querySelectorAll("#status")
+    let index = 0;
+    if(appArray.length==0){}
+else if (appArray.length<5){
+    index =5;
 }
+else{
+    index = appArray.length;
+}
+      for(var i = 0; i < index; i++){
+        date[i].innerHTML = appArray[i].submitDate;
+        start[i].innerHTML = appArray[i].sDate;
+        end[i].innerHTML = appArray.eDate[i];
+        status[i].innerHTML = appArray.status[i];
+      }
+  
+    }
+}}
+function getapp(){
+    console.log( "in get VG bananaphone!");
+    let vgid=document.getElementById("appId").value;
+    var xhr= new XMLHttpRequest();
+     xhr.onreadystatechange= function() {
+        console.log("in ORSC "+ xhr.readyState );
+        if(xhr.readyState==4 && xhr.status==200){
+           console.log(xhr.responseText);
+          var app= JSON.parse(xhr.responseText);
+            loadVG(app);
+        }
+    }
+    
+
 xhr.open("GET","http://localhost:8080/JSONDemo/vg?vgid=" + vgid,true);
  xhr.send();
+ }
+
+ window.onload = function() {
+    getapps();
+    document.getElementById("getapp").addEventListener("click",getapp,false);
  }
